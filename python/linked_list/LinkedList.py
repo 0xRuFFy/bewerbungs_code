586a3b4561f4c512acac9161b0f3f4d708c893d9
Konstantin Opora <konstantinopora@gmail.com>
2022-06-24 19:18:02 +0200

    Update LinkedList.py

--- python/linked_list/LinkedList.py
+++ python/linked_list/LinkedList.py
@@ -1,176 +1,154 @@
 
-from typing import List
+from typing import Any, Iterable
+
 
 class Node:
-    """Node Class
-    -
-    Class for a singel LinkedList Node
-    
-    Variables:
-    
-        - `prevNode` : Node -> reference to the previous Node (None : self -> first Node)
-        - `nextNode` : Node -> reference to the next Node (None : self -> last Node)
-        - `value`           -> value of the Node
-    """
-    def __init__(self, prevNode=None, nextNode=None, value=None) -> None:
-        self.prevNode : Node = prevNode
-        self.nextNode : Node = nextNode
-        self.value = value
-    
-    @property
-    def prevNode(self):
-        """Reference to the previous Node (None : self -> first Node)"""
-        return self.prevNode
-    
-    @property
-    def prevNode(self):
-        """reference to the next Node (None : self -> last Node)"""
-        return self.nextNode
-        
-class LinkedList:
-    """LinkedList Class
-        -
-        
-        Linked List Modul for Python
-
-        Functionality:
-
-            - List can be initialized with any amount and any type of values
-            - the List can be printed with the normal print() function
-            - .get(index) : the value of any Node can be read
-            - .__delitem__(index) : removes Node from the List at the point
-            - .append(value) : appends the linked List with a Node holding a given value
-            - .insert(index, value) : adds a new Node in any given spot with any value
-            - .clear() : deletes all Nodes
-            - .remove(value) : deletes the first Node with the given value
-            
-        Variables:
-
-            - .length : gives the length of the List
-            - .start : head of List
-            
-        @Author Konstantin Opora
-        """
-    
-    def __init__(self, initialValues : List =None) -> None:
-        self.start = None
-        self.length = 0 
-        if initialValues != None:
-            for value in initialValues:
-                self.append(value)
+    def __init__(self, data) -> None:
+        self.data = data
+        self.next: Node = None
     
     def __str__(self) -> str:
-        """returns String representation of the List"""
-        print('{ ', end='')
-        temp = self.start
-        while temp != None:
-            print(temp.value, ' -> ' if temp.nextNode != None else '', end='')
-            temp = temp.nextNode
-        del temp
-        return '}'
-    
-    def get(self, index : int):
-        """If possible returns the value of the Node at a give index."""
-        if 0 <= index <= self.length:
-            temp = self.start
-            for _ in range(index):
-                temp = temp.nextNode
-            return temp.value
-        else:
-            raise IndexError('Index out of range. Given ID: ' +  str(index) + ' | List length: ' + str(self.length))
-    
-    def __delitem__(self, index):
-        """If possible remove the Node at a give index."""
-        if index <= self.length:
-            self.length -= 1
-            if index == 0:
-                self.start = self.start.nextNode
-                if self.start != None:
-                    self.start.prevNode = None
-                return
-            
-            temp = self.start
-            for _ in range(index):
-                temp = temp.nextNode
-            
-            temp.prevNode.nextNode = temp.nextNode
-            if temp.nextNode != None:
-                temp.nextNode.prevNode = temp.prevNode  
-            del temp     
-        else:
-            raise IndexError('Index out of range. Given ID: ' +  str(index) + ' | List length: ' + str(self.length))
-        
-    def append(self, value) -> None:
-        """Appends the List with a Node holding a given value."""
-        self.length += 1     
-        if self.length == 1:
-            self.start = Node(value=value)
-            return     
-        temp = self.start
-        while temp.nextNode != None:
-            temp = temp.nextNode         
-        temp.nextNode = Node(prevNode=temp, value=value)
-        del temp
-        
-    def insert(self, index : int, value) -> None:
-        """Adds a new Node in the given postion with a given value."""
-        if self.length == index == 0:
-            self.length += 1
-            self.start = Node(value=value)
-        elif index <= self.length:
-            self.length += 1          
-            if index == 0:
-                self.start.prevNode = Node(nextNode=self.start, value=value)
-                self.start = self.start.prevNode
-            elif index+1 == self.length:
-                self.append(value)
-            else:
-                temp = self.start
-                for _ in range(index):
-                    temp = temp.nextNode
-                temp.prevNode.nextNode = Node(prevNode=temp.prevNode, nextNode=temp, value=value)
-                temp.prevNode = temp.prevNode.nextNode
-                del temp
-        else:
-            raise IndexError('Index out of range. Given ID: ' +  str(index) + ' | List length: ' + str(self.length))
-        
-    def clear(self) -> None:
-        """Removes all Nodes"""
-        self.length = 0
-        self.start = None
-        
-    def remove(self, value) -> None:
-        """Removes the first occurens of a given value"""
-        temp = self.start
-        i = 0
-        while temp != None:
-            if temp.value == value:
-                self.__delitem__(i)
-                return
-            temp = temp.nextNode
-            i += 1
-        del temp
-        
-    @property
-    def head(self) -> Node:
-        """First element of the List"""
-        return self.start
-    
-    @property
-    def length(self) -> Node:
-        """Length of the List"""
-        return self.length
+        return str(self.data)
 
-if __name__ == '__main__':
+
+class LinkedList:
+    def __init__(self, intitalValues: Iterable = None) -> None:
+        self.head: Node = None
+        self._length: int = 0
+        
+        if intitalValues != None:
+            for val in intitalValues[::-1]:
+                self.push(val)
     
-    mainKnot = LinkedList()                     # print(mainKnot) : { }
-    mainKnot.append('a')                        # print(mainKnot) : { a }
-    mainKnot.insert(0, 'a')                     # print(mainKnot) : { a -> a }
-    mainKnot.__delitem__(0)                     # print(mainKnot) : { a }
-    mainKnot.append('b')                        # print(mainKnot) : { a -> b }
-    mainKnot.clear()                            # print(mainKnot) : { }
-    mainKnot.__init__(['a', 'b', 'j', 'c'])     # print(mainKnot) : { a -> b -> j -> c }
-    mainKnot.remove('j')                        # print(mainKnot) : { a -> b -> c }
+    def __str__(self) -> str:
+        return "{{ {} }}".format(' -> '.join(
+            map(str, list(self))
+        ))
     
-    print(mainKnot.get(2))                      # 'c' 
-    print(mainKnot)                             # { a -> b -> c }
+    def __iter__(self) -> iter:
+        data = [None for _ in range(len(self))]
+        tmp = self.head
+        i = 0
+        while tmp:
+            data[i] = tmp.data
+            tmp = tmp.next
+            i += 1
+        return iter(data)
+
+    def __copy__(self) -> 'LinkedList':
+        return LinkedList(list(self))
+    
+    def __add__(self, data) -> 'LinkedList':
+        new = self.__copy__()
+        new.append(data)
+        return new
+    
+    def __len__(self) -> int:
+        return self._length
+    
+    def __getitem__(self, arg: int | slice) -> Any | 'LinkedList':
+        if type(arg) == int:
+            if arg < 0:
+                return self._get(len(self) + arg)
+            return self._get(arg)
+        elif type(arg) == slice:
+            new = self.__copy__()
+            if arg.step and arg.step < 0:
+                new = new._get_inverted()
+            
+            if arg.start and arg.stop:
+                new = new._get_from_to(arg.start, arg.stop)
+            elif arg.start:
+                new = new._get_last_n(len(self) - arg.start)
+            elif arg.stop:
+                new = new._get_first_n(arg.stop)
+            
+            return new
+        else:
+            raise IndexError('Invalid Index')
+    
+    def _get(self, idx: int):
+        if idx < 0:
+            raise IndexError(f"Index out of range \nGiven: {idx - len(self)} Length: {len(self)}")
+        tmp = self.head
+        i = idx
+        while i > 0:
+            tmp = tmp.next
+            if tmp == None:
+                raise IndexError(f"Index out of range \nGiven: {idx} Length: {len(self)}")
+            i -= 1
+        return tmp.data
+    
+    def _get_inverted(self) -> 'LinkedList':
+        new = self.__copy__()
+        tmp = self.head
+        prev = None
+        while tmp:
+            new.head = Node(tmp.data)
+            new.head.next = prev
+            prev = new.head
+            tmp = tmp.next
+        return new
+    
+    def _get_first_n(self, n) -> 'LinkedList':
+        tmp = self.head
+        new = LinkedList()
+        while n > 0 and tmp:
+            new.append(tmp.data)
+            tmp = tmp.next
+            n -= 1
+        return new
+    
+    def _get_last_n(self, n) -> 'LinkedList':
+        tmp = self._get_inverted().head
+        new = LinkedList()
+        while n > 0 and tmp:
+            new.push(tmp.data)
+            tmp = tmp.next
+            n -= 1
+        return new
+    
+    def _get_from_to(self, f, t) -> 'LinkedList':
+        tmp = self._get_last_n(len(self) - f)
+        return tmp._get_first_n(t - (len(self) - len(tmp)))
+    
+    def _get_all_nth(self, n) -> 'LinkedList':
+        if n == 0:
+            raise ValueError('Cannot step by 0')
+        new = LinkedList()
+        tmp = self.head
+        while tmp:
+            new.append(tmp.data)
+            for _ in range(n):
+                if not tmp:
+                    continue
+                tmp = tmp.next
+        return new
+    
+    def is_empty(self) -> bool:
+        return len(self) == 0
+    
+    def push(self, data) -> None:
+        tmp = self.head
+        newHead = Node(data)
+        newHead.next = tmp
+        self.head = newHead
+        self._length += 1
+    
+    def append(self, data) -> None:
+        tmp = self.head
+        if self.is_empty():
+            self.head = Node(data)
+        else:
+            while tmp.next:
+                tmp = tmp.next
+            tmp.next = Node(data)
+            
+        self._length += 1
+    
+    def append_all(self, data: Iterable) -> None:
+        for val in data:
+            self.append(val)
+            
+    def invert(self) -> None:
+        self = self._get_inverted()
